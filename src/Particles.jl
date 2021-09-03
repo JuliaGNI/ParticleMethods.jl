acc634da28bbe65e8763a2204c56ff1cb52f41a7
Michael Kraus <michael.kraus@ipp.mpg.de>
2021-09-03 14:41:45 +0200

    Import Vlasov-Poisson integrator and bump-on-tail example from ReducedBasisMethods.

--- src/Particles.jl
+++ src/Particles.jl
@@ -1,5 +1,16 @@
 module Particles
 
+"""
+Particles struct.
+fields: positon x, velocity v, weight w
+"""
+struct ParticleList{T}
+    x::Vector{T}
+    v::Vector{T}
+    w::Vector{T}
+end
+
+export ParticleList
 
 
 include("poisson.jl")
@@ -17,4 +28,23 @@ include("splines.jl")
 include("poisson_splines.jl")
 
 export PoissonSolverPBSplines
+
+
+include("integrate_vlasov_poisson.jl")
+
+export VPIntegratorParameters, VPIntegratorCache, integrate_vp
+
+
+include("sampling.jl")
+
+export draw_g_accept_reject, draw_g_importance_sampling, weight_f
+
+
+include("visualisation.jl")
+
+export plot_particles, plot_distribution
+
+
+include("bump_on_tail.jl")
+
 end # module
